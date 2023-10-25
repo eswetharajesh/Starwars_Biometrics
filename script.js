d2c635ee215df2e97ab9476344b00b593ec4ef2f
Swetha Elanchezhiyan <eswetharajesh@gmail.com>
2023-10-25 12:20:56 +0200

    Add project files.

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById("fetchButton").addEventListener("click", getApi);
+
+function getApi() {
+    const characterName = document.getElementById("characterName").value;
+
+    if (characterName.trim() === "") {
+        alert("Please enter a character name.");
+        return;
+    }
+
+    const fullUrl = `https://www.swapi.tech/api/people/?name=${characterName}`;
+
+    fetch(fullUrl)
+        .then((res) => res.json())
+        .then((data) => {
+            if (data.result && data.result.length > 0) {
+                const character = data.result[0];
+                const properties = character.properties;
+
+                let characterInfo = `${characterName} Biometric Data:\n\n`;
+
+                for (const prop in properties) {
+                    characterInfo += `${prop}: ${properties[prop]}\n`;
+                }
+
+                document.getElementById("output").textContent = characterInfo;
+            } else {
+                document.getElementById("output").textContent = "Character not found.";
+            }
+        })
+        .catch((err) => console.log(err));
+}
